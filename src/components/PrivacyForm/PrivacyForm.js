ec149d08c5661e7b8041feb1c5330f87d8b1f98f
cxgarcia <schlaubitzcristobal@gmail.com>
2021-04-23 18:45:29 +0200

    feat: added checkboxes and logic for privacy form

--- src/components/PrivacyForm/PrivacyForm.js
+++ src/components/PrivacyForm/PrivacyForm.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './PrivacyForm.module.scss';
 
 function PrivacyForm({ send }) {
-  return <div></div>;
+  const [userPreferences, setUserPreferences] = useState({
+    trayUpdates: false,
+    otherUpdates: false,
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    send('SUBMIT', { payload: userPreferences });
+  }
+
+  function handleChange(event) {
+    const { name } = event.target;
+    setUserPreferences({ ...userPreferences, [name]: !userPreferences[name] });
+  }
+
+  return (
+    <form onSubmit={handleSubmit} onChange={handleChange}>
+      <input
+        type="checkbox"
+        name="trayUpdates"
+        defaultChecked={userPreferences.trayUpdates}
+      />
+      <input
+        type="checkbox"
+        name="otherUpdates"
+        defaultChecked={userPreferences.otherUpdates}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default PrivacyForm;
--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -6,23 +6,19 @@ import styles from './UserForm.module.scss';
 function UserForm({ send }) {
   const [userInfo, setUserInfo] = useState({});
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    send('SUBMIT', { payload: userInfo });
+  }
+
   //debounced handleChange as to not trigger constant rerenders from state changes when user is inputting values
   const handleChange = debounce(function handleChange(event) {
     const { name, value } = event.target;
     setUserInfo({ ...userInfo, [name]: value });
   }, 500);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    send('SUBMIT', { payload: userInfo });
-  }
-
-  function handleError(event) {
-    console.log(event);
-  }
-
   return (
-    <form onChange={handleChange} onSubmit={handleSubmit} onError={handleError}>
+    <form onSubmit={handleSubmit} onChange={handleChange}>
       <input type="text" name="name" required />
       <input type="text" name="role" />
       <input type="email" name="email" required />
