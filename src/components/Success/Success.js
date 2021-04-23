a2af1fa34c6a8fb1da76f77e7d1bca673e363c33
cxgarcia <schlaubitzcristobal@gmail.com>
2021-04-24 00:34:17 +0200

    feat: the app now displays a json when the form is submitted

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './Success.module.scss';
 
-function Success() {
-  return <div>You made it!!!!</div>;
+function Success({ initialState }) {
+  return (
+    <>
+      <h1 className={styles.formTitle}>Success</h1>
+      <pre className={styles.json}>{JSON.stringify(initialState, null, 4)}</pre>
+    </>
+  );
 }
 
 export default Success;
