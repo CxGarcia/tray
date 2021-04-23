86629fb0e650f7a05e4d5096843d6fe1e266b3c2
cxgarcia <schlaubitzcristobal@gmail.com>
2021-04-23 13:42:44 +0200

    feat: added support for factory pattern in app and added the PrivacySupport component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+import React from 'react';
 import { useMachine } from '@xstate/react';
 import { formStepMachine } from 'state-machines/formStepMachine';
 import UserForm from 'components/UserForm/UserForm';
+import PrivacyForm from 'components/PrivacyForm/PrivacyForm';
 
 import './App.module.scss';
 
-function App() {
-  const [step, send] = useMachine(formStepMachine);
+const steps = {
+  user: UserForm,
+  privacy: PrivacyForm,
+};
 
-  return (
-    <div>
-      <UserForm send={send} />
-    </div>
-  );
+function App() {
+  const [state, send] = useMachine(formStepMachine);
+
+  //the value is the state where the machine is
+  //in this case, the value is the current step of the form
+  const { value } = state;
+
+  //we can dynamically set the current step of the form with a factory component pattern
+  //this pattern also allows us to pass props to our components which is very convenient
+  function stepFactory() {
+    return React.createElement(steps[value], {
+      send,
+    });
+  }
+
+  return <div>{stepFactory()}</div>;
 }
 
 export default App;
---
+++ src/components/PrivacyForm/PrivacyForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './PrivacyForm.module.scss';
+
+function PrivacyForm({ send }) {
+  return <div></div>;
+}
+
+export default PrivacyForm;
