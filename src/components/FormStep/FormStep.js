521fc687809f39cdf3c5291f149cf485a0d50ca4
cxgarcia <schlaubitzcristobal@gmail.com>
2021-04-24 13:58:52 +0200

    feat: added success and failure styling to the FormSteps

--- src/components/FormStep/FormStep.js
+++ src/components/FormStep/FormStep.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './FormStep.module.scss';
 
-function FormStep({ steps, value }) {
+function FormStep({ value }) {
   function renderSteps() {
-    const steps = ['user', 'privacy', 'done'];
+    const steps = ['user', 'privacy'];
     const idxOfActiveStep = steps.indexOf(value);
 
     return steps.map((step, idx) => {
-      const active = idx <= idxOfActiveStep;
+      const active = idx <= idxOfActiveStep || idxOfActiveStep === -1;
 
       return (
         <h3
@@ -23,7 +23,30 @@ function FormStep({ steps, value }) {
     });
   }
 
-  return <>{renderSteps()}</>;
+  function renderDone() {
+    const success = value === 'success';
+    const failure = value === 'failure';
+
+    return (
+      <h3
+        key="done"
+        className={`
+          ${styles.step}
+          ${success && styles.success}
+          ${failure && styles.failure}
+          `}
+      >
+        {failure ? 'error' : success ? 'success' : 'done'}
+      </h3>
+    );
+  }
+
+  return (
+    <>
+      {renderSteps()}
+      {renderDone()}
+    </>
+  );
 }
 
 export default FormStep;
