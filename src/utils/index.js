3a86fceb0998b1f81039e431ddce8b4b165497aa
cxgarcia <schlaubitzcristobal@gmail.com>
2021-04-26 16:15:05 +0200

    feat: deep clone the submitted object as to not mutate the original one

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,24 @@
+export const deepClone = (source) => {
+  if (typeof source !== 'object' || source === null) {
+    return source;
+  }
+
+  if (Array.isArray(source)) {
+    const copyArr = source.map((el) => {
+      return deepClone(el);
+    });
+    return copyArr;
+  }
+
+  const copy = {};
+
+  for (var key in source) {
+    copy[key] = deepClone(source[key]);
+  }
+
+  return copy;
+};
+
 export const debounce = (fn, timeout = 1000) => {
   let timeoutRef = null;
 
