5c188612aa381242710c9eb4558e810255f8dbad
cxgarcia <schlaubitzcristobal@gmail.com>
2021-04-26 15:58:13 +0200

    docs: info about running tests, the decision making process, and a more info on xstate in the readme section of the repo

--- src/state-machines/formStepMachine.js
+++ src/state-machines/formStepMachine.js
@@ -20,19 +20,17 @@ export const formStepMachine = Machine({
   },
   //the initial state (step) of our form
   initial: 'user',
-  //this part lays out all the states our machine can be in
+  //Next we define all the states of the machine
   //it is important to note that the machine can only be in one state at a time
   //this is very convenient since it prevents us from falling into impossible/undesireable states
   states: {
     user: {
-      //here we can also specify how our machine will transition between states
+      //the on is the event that allows us to transition between states
       //for example, once the form is submitted, the state machine transitions into the privacy state and so on
-      //when it transitions, it also updates the extended state
       on: {
         SUBMIT: {
           target: 'privacy',
           //when the form is submitted, it has already been validated
-          //thus we can overwrite whatever we have in our initial ctx
           actions: assign({ user: (_, event) => event.payload }),
         },
       },
@@ -43,6 +41,7 @@ export const formStepMachine = Machine({
           target: 'loading',
           actions: assign({ privacy: (_, event) => event.payload }),
         },
+        //another possible event, is if we go back to previous step which is handled by the BACK event
         BACK: {
           target: 'user',
           actions: assign({ privacy: (_, event) => event.payload }),
@@ -50,21 +49,20 @@ export const formStepMachine = Machine({
       },
     },
     loading: {
-      //here I have mocked a submission of the form via the api service
-      //the service randomizes the promise resolve and reject
-
+      //here I have mocked a submission of the form via an api
+      //the service randomizes if the promise resolves or rejects
       invoke: {
         id: 'submit-form',
         src: (ctx) => submit(ctx),
         //if the promise resolves, we go into the success state
-        //at this point we can dispatch a redux action and update our global state
+        //at this point we can dispatch a redux action and update our global store
         onDone: {
           target: 'success',
-          //whatever is returned by our server, is saved into the success object
+          //whatever is returned by the server, is saved into the success object
           actions: assign({ success: (_, event) => event.data }),
         },
         //if the promise rejects, we go into the failure state
-        //from this state the only viable option is to retry submitting the form
+        //from this state the only option is to retry submitting the form
         onError: 'failure',
       },
     },
@@ -74,7 +72,7 @@ export const formStepMachine = Machine({
       },
     },
     success: {
-      //TODO - on success, dispatch redux action
+      //on success, dispatch redux action so we can update our global store
       type: 'final',
     },
   },
